ddfd0a070d677c7b62b190fc0136b08c49030fe2
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-09-03 04:03:39 -0300

    feat: :sparkles: responsive notification

--- src/components/atoms/Notification/index.jsx
+++ src/components/atoms/Notification/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./index.module.scss";
 import NotificationIcon from "../../../asset/Notification.svg";
 const Notification = () => {
-  return <img src={NotificationIcon} alt="Notificação" />;
+  return (
+    <img src={NotificationIcon} alt="Notificação" className={styles.icon} />
+  );
 };
 
 export default Notification;

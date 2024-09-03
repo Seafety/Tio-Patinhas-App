4365ae1c73ac86e5a5be93b23aef921f1f593d6c
Lucy <127787631+vonBoutti@users.noreply.github.com>
2024-09-03 19:15:54 -0300

    feat: 🙌 components part2

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import FinancialDashboard from "../../components/organisms/FinancialDashboard";
+import CarteiraOverview from "../../components/molecules/CarteiraOverview";
+import MarketStats from "../../components/molecules/MarketStats";
+import styles from "./index.module.scss";
 
 const Dashboard = () => {
-  return <div>Dashboard
+  return <div>
     <FinancialDashboard />
+    <div className={styles.container}>
+    <CarteiraOverview />
+    <MarketStats />
+    </div>
   </div>;
 };
 

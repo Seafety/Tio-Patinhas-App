9cc0f5ddb2f8d11b7e18fd5d0022d813693cb8f6
Lucy <127787631+vonBoutti@users.noreply.github.com>
2024-09-04 19:50:17 -0300

    feat: 💤 first commit!

---
+++ src/components/molecules/Historico/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styles from './index.module.scss';
+import arrowUp from '../../../asset/arrowUp.svg';
+import arrowDown from '../../../asset/arrowDown.svg';
+
+const Historico = () => {
+  return (
+    <div className={styles.historico}>
+      <div className={styles.header}>
+        <h2>Histórico</h2>
+      </div>
+      <div className={styles.transacao}>
+        <div className={styles.data}>
+          <span className={styles.mes}>Set</span>
+          <span className={styles.dia}>04</span>
+        </div>
+        <div className={styles.detalhes}>
+          <span className={styles.nome}>Ronald Mcduck</span>
+          <span className={styles.tipo}>Recebido</span>
+        </div>
+        <div className={styles.valorContainer}>
+            <img src={arrowUp} className={styles.iconeRecebido} />
+          <span className={styles.valorRecebido}>R$559</span>
+        </div>
+      </div>
+      <div className={styles.transacao}>
+        <div className={styles.data}>
+          <span className={styles.mes}>Ago</span>
+          <span className={styles.dia}>21</span>
+        </div>
+        <div className={styles.detalhes}>
+          <span className={styles.nome}>Moana Princess</span>
+          <span className={styles.tipo}>Transferência</span>
+        </div>
+        <div className={styles.valorContainer}>
+          <img src={arrowDown} className={styles.iconePago} />
+          <span className={styles.valorPago}>R$98</span>
+        </div>
+      </div>
+      <div className={styles.transacao}>
+        <div className={styles.data}>
+          <span className={styles.mes}>Jun</span>
+          <span className={styles.dia}>19</span>
+        </div>
+        <div className={styles.detalhes}>
+          <span className={styles.nome}>Mickey Mouse</span>
+          <span className={styles.tipo}>Transferência</span>
+        </div>
+        <div className={styles.valorContainer}>
+          <img src={arrowDown} className={styles.iconePago} />
+          <span className={styles.valorPago}>R$98</span>
+        </div>
+      </div>
+      <div className={styles.transacao}>
+        <div className={styles.data}>
+          <span className={styles.mes}>Jun</span>
+          <span className={styles.dia}>12</span>
+        </div>
+        <div className={styles.detalhes}>
+          <span className={styles.nome}>Mickey Mouse</span>
+          <span className={styles.tipo}>Recebido</span>
+        </div>
+        <div className={styles.valorContainer}>
+          <img src={arrowUp} className={styles.iconeRecebido} />
+          <span className={styles.valorRecebido}>R$9.821</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Historico;
--- src/pages/transactions/index.jsx
+++ src/pages/transactions/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SaldoTotal from "../../components/molecules/SaldoTotal";
+import LimitesDiarios from "../../components/molecules/LimitesDiarios";
+import Historico from "../../components/molecules/Historico";
+import styles from "./index.module.scss";
 
 const Transactions = () => {
-  return <div>Transactions</div>;
+  return <div className={styles.transactions}>
+  <SaldoTotal />
+  <LimitesDiarios />
+  <Historico /></div>;
 };
 
 export default Transactions;

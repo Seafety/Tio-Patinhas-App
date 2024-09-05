03b1a9ba0793bc93d7d94c61a10d0b63cf44c1ea
Mcf-hub <ferreira.maria2322@gmail.com>
2024-09-05 20:41:01 -0300

    basics

--- src/pages/market/all/index.jsx
+++ src/pages/market/all/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CryptoSections from "../../../components/organisms/CryptoSections";
+import CryptoTable from "../../../components/organisms/CryptoTable";
+
 
 const AllCoins = () => {
-  return <div>AllCoins</div>;
+  return (
+    <div>...</div>
+  );
 };
 
 export default AllCoins;
--- src/pages/market/index.jsx
+++ src/pages/market/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import CryptoSections from "../../components/organisms/CryptoSections";
+import CryptoTable from "../../components/organisms/CryptoTable";
+
 
 const Market = () => {
-  return <div>Market</div>;
+  return (
+    <div>
+    
+    <CryptoSections />
+    <CryptoTable />
+  </div>
+  )
 };
 
 export default Market;

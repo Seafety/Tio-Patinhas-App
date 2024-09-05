efbf5547fd0587f14c4e470e197077172168b9cc
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-09-05 01:58:53 -0300

    feat: :sparkles: adding profile page structure
    adding profile page structure

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import styles from "./index.module.scss";
+import PerfilData from "../../components/organisms/PerfilData";
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container_title}>
+        <h1>Perfil de Investidor</h1>
+      </div>
+      <div className={styles.container_left_up}>
+        <PerfilData />
+      </div>
+      <div className={styles.container_left_low}>
+        <PerfilData />
+      </div>
+      <div className={styles.container_right_up}>
+        <PerfilData />
+      </div>
+      <div className={styles.container_right_low}>
+        <PerfilData />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
